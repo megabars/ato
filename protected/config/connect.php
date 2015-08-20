860ddf11bb83b3f340cc3f90c5cb836711da35a9
Мезенцев Иван <mezencevip@fisb.ru>
2015-08-20 16:54:50 +0600

    new

--- protected/config/connect.php
+++ protected/config/connect.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'connectionString' => 'pgsql:host=192.168.150.249;port=5432;dbname=dbname',
+    'connectionString' => 'pgsql:host=192.168.150.248;port=5432;dbname=dbname',
     'emulatePrepare' => true,
     'username' => 'test',
     'password' => 'test',
